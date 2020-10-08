8f5bf8e4e3dcbc0fac81c096ff0ca88a0f22448b
Liniker Oliveira <linikerenem@gmail.com>
2020-10-08 02:26:04 -0300

    Criando método comprimentar..

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:
-    pass
+    def comprimentar(self):
+        return f'Olá {id(self)}'
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(id(p))
+    print(p.comprimentar())
+
