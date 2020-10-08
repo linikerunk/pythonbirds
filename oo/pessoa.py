0fe2dbecac60a1f16496f0f4ad3cbede4c67fd84
Liniker Oliveira <linikerenem@gmail.com>
2020-10-08 02:30:11 -0300

    Criado atributo de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,16 @@
 class Pessoa:
+
+    def __init__(self, nome = None, idade=35):
+        self.nome = nome
+
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("Luciano")
     print(id(p))
     print(p.comprimentar())
-
+    print(p.nome)
+    p.nome = "Liniker"
+    print(p.nome)
+    print(p.idade)
